dfa831db0803bc0ee63a8b5757114e77e3b155cc
yiwang <468731422@qq.com>
2026-02-16 02:06:33 +0800

    fix-markdown-rendering-regex-in-webview

--- src/views/aiChatView.ts
+++ src/views/aiChatView.ts
@@ -302,11 +302,11 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
         function renderMarkdown(text) {
             let html = esc(text);
             
-            html = html.replace(/\`\`\`(\\w*)\\n([\\s\\S]*?)\`\`\`/g, function(match, lang, code) {
+            html = html.replace(/```(\w*)\n([\s\S]*?)```/g, function(match, lang, code) {
                 return '<pre><code class="language-' + lang + '">' + code.trim() + '</code></pre>';
             });
             
-            html = html.replace(/\`([^\`]+)\`/g, '<code>$1</code>');
+            html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
             
             html = html.replace(/\*\*([^\*]+)\*\*/g, '<strong>$1</strong>');
             html = html.replace(/\*([^\*]+)\*/g, '<em>$1</em>');
@@ -318,24 +318,24 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
             html = html.replace(/^> (.+)$/gm, '<blockquote>$1</blockquote>');
             
             html = html.replace(/^[-*] (.+)$/gm, '<li>$1</li>');
-            html = html.replace(/(<li>.*<\\/li>\\n?)+/g, '<ul>$&</ul>');
+            html = html.replace(/(<li>.*<\/li>\n?)+/g, '<ul>$&</ul>');
             
-            html = html.replace(/\\[([^\\]]+)\\]\\(([^)]+)\\)/g, '<a href="$2">$1</a>');
+            html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
             
             html = html.replace(/^---$/gm, '<hr>');
             
-            html = html.replace(/\\n\\n/g, '</p><p>');
+            html = html.replace(/\n\n/g, '</p><p>');
             html = '<p>' + html + '</p>';
-            html = html.replace(/<p><\\/p>/g, '');
+            html = html.replace(/<p><\/p>/g, '');
             html = html.replace(/<p>(<h[1-6]>)/g, '$1');
-            html = html.replace(/(<\\/h[1-6]>)<\\/p>/g, '$1');
+            html = html.replace(/(<\/h[1-6]>)<\/p>/g, '$1');
             html = html.replace(/<p>(<pre>)/g, '$1');
-            html = html.replace(/(<\\/pre>)<\\/p>/g, '$1');
+            html = html.replace(/(<\/pre>)<\/p>/g, '$1');
             html = html.replace(/<p>(<ul>)/g, '$1');
-            html = html.replace(/(<\\/ul>)<\\/p>/g, '$1');
+            html = html.replace(/(<\/ul>)<\/p>/g, '$1');
             html = html.replace(/<p>(<blockquote>)/g, '$1');
-            html = html.replace(/(<\\/blockquote>)<\\/p>/g, '$1');
-            html = html.replace(/<p>(<hr>)<\\/p>/g, '$1');
+            html = html.replace(/(<\/blockquote>)<\/p>/g, '$1');
+            html = html.replace(/<p>(<hr>)<\/p>/g, '$1');
             
             return html;
         }
